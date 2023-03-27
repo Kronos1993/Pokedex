e78e252a0cc36dd70a48444706d0a36684e59314
MGUERRAL <marcos.guerra@smartmatic.com>
2023-03-26 21:48:44 -0500

    binding adapters renamed

--- app/src/main/java/com/kronos/pokedex/binding_adapters/ImageViewBindingAdapter.kt
+++ app/src/main/java/com/kronos/pokedex/binding_adapters/ImageViewBindingAdapter.kt
@@ -6,8 +6,8 @@ import com.bumptech.glide.Glide
 import com.kronos.pokedex.R
 
 
-@BindingAdapter("app:handle_pokemon_type")
-fun handleType(view: ImageView, typeName: String) = view.run {
+@BindingAdapter("app:handle_type")
+fun handleType(view: ImageView, typeName: String?) = view.run {
     if (typeName != null) {
         when {
             typeName.equals("normal", false) -> {
@@ -64,14 +64,19 @@ fun handleType(view: ImageView, typeName: String) = view.run {
             typeName.equals("fairy", false) -> {
                 view.setBackgroundResource(R.drawable.ic_pokemon_type_fairy)
             }
-            typeName.equals("shadow", false) -> {
-                view.setBackgroundResource(R.drawable.ic_pokemon_type_icons)
+        }
+    }
+}
+
+@BindingAdapter("app:handle_move_priority")
+fun handleMovePriority(view: ImageView, typePriority: Int?) = view.run {
+    if (typePriority != null) {
+        when (typePriority) {
+            1 -> {
+                view.setBackgroundResource(R.drawable.ic_check)
             }
-            typeName.equals("unknown", false) -> {
-                view.setBackgroundResource(R.drawable.ic_pokemon_type_icons)
-            }
-            else -> {
-                view.setBackgroundResource(R.drawable.ic_pokemon_type_icons)
+            0 -> {
+                view.setBackgroundResource(R.drawable.ic_cancel)
             }
         }
     }
