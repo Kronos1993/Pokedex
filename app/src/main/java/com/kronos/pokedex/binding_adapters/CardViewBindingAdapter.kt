9b34b9177bfa1563f085a50dbb5b9ac80d0cd530
MGUERRAL <marcos.guerra@smartmatic.com>
2023-10-02 11:20:59 -0500

    berry tooltip added

--- app/src/main/java/com/kronos/pokedex/binding_adapters/CardViewBindingAdapter.kt
+++ app/src/main/java/com/kronos/pokedex/binding_adapters/CardViewBindingAdapter.kt
@@ -1,9 +1,11 @@
 package com.kronos.pokedex.binding_adapters
 
+import android.os.Build
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.google.android.material.card.MaterialCardView
 import com.kronos.pokedex.R
+import com.kronos.pokedex.domian.model.item.BerryFlavor
 
 @BindingAdapter("is_hidden_ability")
 fun isHiddenAbility(view: MaterialCardView, hidden: Boolean) = view.run {
@@ -46,7 +48,29 @@ fun handleBerryPotency(view: MaterialCardView, potency: Int?) = view.run {
                 view.setCardBackgroundColor(ContextCompat.getColor(view.context, R.color.berry_potency_med))
             }
             potency > 10 -> {
-                view.setCardBackgroundColor(ContextCompat.getColor(view.context, R.color.berry_potency_hight))
+                view.setCardBackgroundColor(ContextCompat.getColor(view.context, R.color.berry_potency_high))
+            }
+        }
+    }
+}
+
+@BindingAdapter("handle_berry_potency_description")
+fun handleBerryPotencyDescription(view: MaterialCardView, flavor: BerryFlavor?) = view.run {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        if (flavor != null) {
+            when {
+                flavor.potency == 0 -> {
+                    view.tooltipText = String.format(view.context.getString(R.string.berry_potency_none),flavor.flavor.name)
+                }
+                flavor.potency in 1..5 -> {
+                    view.tooltipText = String.format(view.context.getString(R.string.berry_potency_low),flavor.flavor.name)
+                }
+                flavor.potency in 5..10 -> {
+                    view.tooltipText = String.format(view.context.getString(R.string.berry_potency_medium),flavor.flavor.name)
+                }
+                flavor.potency > 10 -> {
+                    view.tooltipText = String.format(view.context.getString(R.string.berry_potency_high),flavor.flavor.name)
+                }
             }
         }
     }
