2764d4108883183eeff1d409e694fc1c15f7efe6
mguerral <mguerral>
2023-11-21 11:41:08 -0500

    Code refactor

--- app/src/main/java/com/kronos/pokedex/ui/pokemon/detail/PokemonDetailViewModel.kt
+++ app/src/main/java/com/kronos/pokedex/ui/pokemon/detail/PokemonDetailViewModel.kt
@@ -135,40 +135,40 @@ class PokemonDetailViewModel @Inject constructor(
 
     var pokemonDescription = ObservableField<String?>()
 
-    var pokemonName = ObservableField<String?>()
+    private var pokemonName = ObservableField<String?>()
 
     var pokemonGenera = ObservableField<String?>()
     var pokemonGenderPossibility = ObservableField<GenderPossibility?>()
 
-    var showMove = ObservableField<String?>()
-    var buttonSelected = ObservableField<String?>()
+    private var showMove = ObservableField<String?>()
+    private var buttonSelected = ObservableField<String?>()
 
-    fun postPokemonInfo(pokemonInfo: PokemonInfo?) {
+    private fun postPokemonInfo(pokemonInfo: PokemonInfo?) {
         _pokemonInfo.postValue(pokemonInfo)
     }
 
-    fun postSpecieInfo(specie: SpecieInfo?) {
+    private fun postSpecieInfo(specie: SpecieInfo?) {
         _specieInfo.postValue(specie)
     }
 
-    fun postPokemonStats(stats: List<Stat>) {
+    private fun postPokemonStats(stats: List<Stat>) {
         _pokemonStats.postValue(stats)
     }
 
-    fun postPokemonMoves(moves: List<MoveList>) {
+    private fun postPokemonMoves(moves: List<MoveList>) {
         _pokemonMovesToShow.postValue(moves.sortedBy {
             it.order
         })
     }
 
     private fun groupMoves(moves: List<MoveList>){
-        var levelUp = mutableListOf<MoveList>()
-        var tutor = mutableListOf<MoveList>()
-        var tm = mutableListOf<MoveList>()
-        var egg = mutableListOf<MoveList>()
-        var other = mutableListOf<MoveList>()
+        val levelUp = mutableListOf<MoveList>()
+        val tutor = mutableListOf<MoveList>()
+        val tm = mutableListOf<MoveList>()
+        val egg = mutableListOf<MoveList>()
+        val other = mutableListOf<MoveList>()
 
-        var moveDetail: MoveDetail? = null
+        var moveDetail: MoveDetail?
         for (move in moves){
             if (move.moveDetails.isNotEmpty()){
                 moveDetail = move.moveDetails[0]
@@ -203,15 +203,15 @@ class PokemonDetailViewModel @Inject constructor(
         _pokemonEvolutionChain.postValue(evolutionChain)
     }
 
-    fun postPokemonEvolutionChainList(evolutionChainList: List<ChainLink>) {
+    private fun postPokemonEvolutionChainList(evolutionChainList: List<ChainLink>) {
         _pokemonEvolutionList.postValue(evolutionChainList)
     }
 
-    fun postPokemonSprites(pokemonSprites: List<Pair<String, String>>) {
+    private fun postPokemonSprites(pokemonSprites: List<Pair<String, String>>) {
         _pokemonSpritesUrl.postValue(pokemonSprites)
     }
 
-    fun postPokemonOtherForms(pokemonOtherForms: List<Pair<String, String>>) {
+    private fun postPokemonOtherForms(pokemonOtherForms: List<Pair<String, String>>) {
         _pokemonOtherFormsUrl.postValue(pokemonOtherForms)
     }
 
@@ -223,7 +223,7 @@ class PokemonDetailViewModel @Inject constructor(
         _moveInfo.postValue(moveInfo)
     }
 
-    fun postPokemonGames(list: List<Game>) {
+    private fun postPokemonGames(list: List<Game>) {
         _pokemonGames.postValue(list)
     }
 
@@ -234,10 +234,9 @@ class PokemonDetailViewModel @Inject constructor(
             pokemonName.set(null)
             pokemonGenera.set(null)
             pokemonGenderPossibility.set(GenderPossibility())
-            var pokemonInfo: PokemonInfo? = null
             statsTotal.set(0)
 
-            pokemonInfo = if (urlProvider.extractIdFromUrl(pokemonList.url) != null) {
+            val pokemonInfo: PokemonInfo? = if (urlProvider.extractIdFromUrl(pokemonList.url) != null) {
                 pokemonRemoteRepository.getPokemonInfo(urlProvider.extractIdFromUrl(pokemonList.url))
             } else {
                 pokemonRemoteRepository.getPokemonInfo(pokemonList.name)
@@ -245,8 +244,8 @@ class PokemonDetailViewModel @Inject constructor(
 
             //var specie = pokemonSpecieRemoteRepository.getSpecieInfo(pokemonInfo.id)
 
-            if (pokemonInfo.specieInfo != null && !pokemonInfo.specieInfo.name.isNullOrEmpty()) {
-                var genderPossibility = GenderPossibility()
+            if (pokemonInfo!!.specieInfo != null && pokemonInfo.specieInfo.name.isNotEmpty()) {
+                val genderPossibility = GenderPossibility()
                 genderPossibility.getPossibilities(pokemonInfo.specieInfo.genderRate)
                 pokemonGenderPossibility.set(genderPossibility)
                 postSpecieInfo(pokemonInfo.specieInfo)
@@ -264,42 +263,42 @@ class PokemonDetailViewModel @Inject constructor(
             postPokemonStats(pokemonInfo.stats)
             postPokemonGames(pokemonInfo.games)
 
-            var pokemonSprite = mutableListOf<Pair<String, String>>()
+            val pokemonSprite = mutableListOf<Pair<String, String>>()
             pokemonInfo.sprites.frontHome.let {
-                if (!it.isNullOrEmpty()) {
+                if (it.isNotEmpty()) {
                     pokemonSprite.add(Pair(it, context.getString(R.string.home)))
                 }
             }
             pokemonInfo.sprites.frontHomeShiny.let {
-                if (!it.isNullOrEmpty()) {
+                if (it.isNotEmpty()) {
                     pokemonSprite.add(Pair(it, context.getString(R.string.home_shiny)))
                 }
             }
             pokemonInfo.sprites.frontDefault.let {
-                if (!it.isNullOrEmpty()) {
+                if (it.isNotEmpty()) {
                     pokemonSprite.add(Pair(it, context.getString(R.string.front)))
                 }
             }
             pokemonInfo.sprites.frontFemale.let {
-                if (!it.isNullOrEmpty()) {
+                if (it.isNotEmpty()) {
                     pokemonSprite.add(Pair(it, context.getString(R.string.female)))
                 }
             }
             pokemonInfo.sprites.frontShiny.let {
-                if (!it.isNullOrEmpty()) {
+                if (it.isNotEmpty()) {
                     pokemonSprite.add(Pair(it, context.getString(R.string.front_shiny)))
                 }
             }
             pokemonInfo.sprites.frontShinyFemale.let {
-                if (!it.isNullOrEmpty()) {
+                if (it.isNotEmpty()) {
                     pokemonSprite.add(Pair(it, context.getString(R.string.female_shiny)))
                 }
             }
             postPokemonSprites(pokemonSprite)
 
-            var pokemonOtherForms = mutableListOf<Pair<String, String>>()
+            val pokemonOtherForms = mutableListOf<Pair<String, String>>()
             pokemonInfo.specieInfo.varieties.forEach {
-                if (!it.pokemon.name.isNullOrEmpty() && pokemonInfo.name != it.pokemon.name) {
+                if (it.pokemon.name.isNotEmpty() && pokemonInfo.name != it.pokemon.name) {
                     pokemonOtherForms.add(
                         Pair(
                             it.pokemon.url,
@@ -368,16 +367,16 @@ class PokemonDetailViewModel @Inject constructor(
     fun getPokemonEvolution(){
         viewModelScope.launch (Dispatchers.IO){
             if (!pokemonInfo.value?.specieInfo?.evolutionChain?.url.isNullOrEmpty()) {
-                var evolChain = pokemonEvolutionChainRemoteRepository.getEvolutionChain(
+                val evolChain = pokemonEvolutionChainRemoteRepository.getEvolutionChain(
                     urlProvider.extractIdFromUrl(pokemonInfo.value?.specieInfo?.evolutionChain.let {
-                        if (it != null && !it.url.isNullOrEmpty())
+                        if (it != null && it.url.isNotEmpty())
                             it.url
                         else
                             "0"
                     })
                 )
                 postPokemonEvolutionChain(evolChain)
-                var evoList = mutableListOf(evolChain.chain!!)
+                val evoList = mutableListOf(evolChain.chain!!)
                 postPokemonEvolutionChainList(handleEvolutionChain(evoList, evolChain.chain!!))
             }
         }
@@ -394,9 +393,9 @@ class PokemonDetailViewModel @Inject constructor(
                 this
             }
         }
-        return if (chain?.evolvesTo!!.isNotEmpty()) {
+        return if (chain.evolvesTo.isNotEmpty()) {
             var i = ChainLink()
-            for (item in chain?.evolvesTo!!) {
+            for (item in chain.evolvesTo) {
                 i = item
                 i.evolvesFrom = chain.species.name
                 if(pokemonInfo.value?.name.equals(i.species.name))
@@ -412,9 +411,7 @@ class PokemonDetailViewModel @Inject constructor(
     fun loadAbilityInfo(ability: NamedResourceApi) {
         viewModelScope.launch(Dispatchers.IO) {
             loading.postValue(true)
-            var abilityInfo: AbilityInfo? = null
-
-            abilityInfo = if (urlProvider.extractIdFromUrl(ability.url) != null) {
+            val abilityInfo = if (urlProvider.extractIdFromUrl(ability.url) != null) {
                 abilityRemoteRepository.getAbility(urlProvider.extractIdFromUrl(ability.url))
             } else {
                 abilityRemoteRepository.getAbility(ability.name)
@@ -427,9 +424,7 @@ class PokemonDetailViewModel @Inject constructor(
     fun loadMoveInfo(move: NamedResourceApi) {
         viewModelScope.launch(Dispatchers.IO) {
             loading.postValue(true)
-            var moveInfo: MoveInfo? = null
-
-            moveInfo = if (urlProvider.extractIdFromUrl(move.url) != null) {
+            val moveInfo = if (urlProvider.extractIdFromUrl(move.url) != null) {
                 moveRemoteRepository.getMove(urlProvider.extractIdFromUrl(move.url))
             } else {
                 moveRemoteRepository.getMove(move.name)
